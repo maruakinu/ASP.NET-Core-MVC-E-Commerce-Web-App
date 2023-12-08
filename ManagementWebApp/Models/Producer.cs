5de2c231d0fbc81e7922a716c5802089624d50fc
maruakinu <marloaquino080621@gmail.com>
2023-12-08 23:31:44 +0800

    Adding Required Field Exception

--- ManagementWebApp/Models/Producer.cs
+++ ManagementWebApp/Models/Producer.cs
@@ -10,12 +10,15 @@ namespace ManagementWebApp.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture URL")]
+        [Required(ErrorMessage = "Profile Picture is Required")]
         public string profilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "FullName is Required")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is Required")]
         public string Bio { get; set; }
 
         //Relationships
